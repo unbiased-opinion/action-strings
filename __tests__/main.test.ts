3e19fa323c7fa3ce4f536c73cbde6ab5c4300ecd
Lukasz Wisniewski <lw540513@gmail.com>
2023-03-02 23:19:31 +0000

    Assert action output

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,19 +1,14 @@
 import { expect } from '@jest/globals'
-import { actionRun } from './utils/helpers';
 import { toLower } from '../src/methods/toLower';
 
 describe(`methods`, () => {
-  const value = 'AHh, hELlO ThERE!';
-  const expectedValue = 'ahh, hello there!';
-  const method = 'toLower';
+    const value = 'AHh, hELlO ThERE!';
+    const expectedValue = 'ahh, hello there!';
+    const method = 'toLower';
 
-  describe(method, () => {
-    it(`transforms the string`, () => {
-      expect(toLower(value)).toBe(expectedValue);
+    describe(method, () => {
+        it(`transforms the string`, () => {
+            expect(toLower(value)).toBe(expectedValue);
+        });
     });
-
-    it(`${method} runs`, () => {
-      expect(actionRun(method, value)).toContain(expectedValue);
-    })
-  });
 });
